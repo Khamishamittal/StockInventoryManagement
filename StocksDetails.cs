a6e5c80c28c0ad2fd5adcf318c7d3a7a2b7a7803
“Khamishamittal” <Khamishamittal090@gmail.com>
2023-02-21 21:29:13 +0530

    Various Types of Stocks Added in Inventory

---
+++ StocksDetails.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockInventoryManagement
+{
+
+    public class Stocks
+    {
+        public string StockName;
+        public int shares;
+        public int Price;
+    }
+    public class StockDetails
+    {
+        public List<Stocks> stocks;
+    }
+}
