2375937fab6bdaa19bb30a4f44378bd9686a72d3
“Khamishamittal” <Khamishamittal090@gmail.com>
2023-02-21 21:28:42 +0530

    Various Types of Stocks Added in Inventory

---
+++ FetchStocks.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace StockInventoryManagement
+{
+    public class FetchStocks
+    {
+        public StockDetails Read(string path)
+        {
+            using (StreamReader file  = new StreamReader(path))
+            {
+                try
+                {
+                    string json = file.ReadToEnd();
+                    return JsonConvert.DeserializeObject<StockDetails>(json);
+                }
+                catch(Exception ex)
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
